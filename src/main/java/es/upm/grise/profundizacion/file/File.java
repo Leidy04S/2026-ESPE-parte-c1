5406a73598305a92bf5f0669214596e54aaed0a7
Leidy04S <leidysaraguro691@gmail.com>
2026-02-03 09:46:46 -0500

    Implementa código según especificación usando LLM

--- src/main/java/es/upm/grise/profundizacion/file/File.java
+++ src/main/java/es/upm/grise/profundizacion/file/File.java
@@ -1,6 +1,8 @@
 package es.upm.grise.profundizacion.file;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.CRC32;
 
 public class File {
 
@@ -11,14 +13,26 @@ public class File {
 	 * Constructor
 	 */
     public File() {
-        
+        this.content = new ArrayList<>();
+        this.type = null;
     }
 
 	/*
 	 * Method to code / test
 	 */
-    public void addProperty(char[] content) {
-
+    public void addProperty(char[] newcontent) throws InvalidContentException, WrongFileTypeException {
+        
+        if (newcontent == null) {
+            throw new InvalidContentException("Content cannot be null");
+        }
+        
+        if (this.type == FileType.IMAGE) {
+            throw new WrongFileTypeException("Cannot add property to an IMAGE file");
+        }
+        
+        for (char c : newcontent) {
+            this.content.add(c);
+        }
     }
 
 	/*
@@ -26,8 +40,22 @@ public class File {
 	 */
     public long getCRC32() {
     	
-        return 0L;
+        if (this.content.isEmpty()) {
+            return 0L;
+        }
         
+        // Convert ArrayList<Character> to byte[]
+        byte[] bytes = new byte[this.content.size()];
+        for (int i = 0; i < this.content.size(); i++) {
+            // Use only the least significant byte (values in range [0, 255])
+            bytes[i] = (byte) (this.content.get(i) & 0xFF);
+        }
+        
+        // Calculate CRC32 using Java's built-in CRC32 class
+        CRC32 crc = new CRC32();
+        crc.update(bytes);
+        
+        return crc.getValue();
     }
     
     
---
+++ src/main/java/es/upm/grise/profundizacion/file/InvalidContentException.java
@@ -0,0 +1,9 @@
+package es.upm.grise.profundizacion.file;
+
+public class InvalidContentException extends Exception {
+
+	public InvalidContentException(String message) {
+		super(message);
+	}
+
+}
